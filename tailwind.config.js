7238509f91c553caea095001092cb0edb2ea9827
Yuri Bizh <qwantix17@gmail.com>
2023-06-23 18:11:46 +0300

    Co-authored-by: maxim2005ig@gmail.com <maxim2005ig@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Gloock"],
+      },
+      backgroundImage: {
+        first:
+          "url('https://cdn.discordapp.com/attachments/1043641455643742208/1121819571926605934/161724273_123307499737155_4550365991362419331_n.png')",
+      },
+    },
   },
   plugins: [],
-}
-
+};
